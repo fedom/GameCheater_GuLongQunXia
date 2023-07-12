174a6c83d357a5ec1d2c920a3a800fe44a04a7af
leohl <hl1008@gmail.com>
2023-07-11 22:27:16 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Description
+GuLongQunXiaZhuan is a old Chinese RPG game. Unlike most of the old games, which can be simply modified through tool like CheatEngine, this game has encrypt its value so its not easy to cheat. This simple tool just use the same encryption algorithm as the game. So you can calculate the encrypted value and search it through CheatEngine as usual.
+
+## Build
+cmake -B build && cmake --build build
+
+## Run
+./build/src/GameCheater
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+unsigned int rol_ignore_carry(unsigned int n, unsigned int shift) {
+    assert(shift < 32);
+    unsigned int part1 = n << shift;
+    unsigned int part2 = n >> (32 - shift);
+    return part1 | part2;
+}
+
+unsigned int ror_ignore_carry(unsigned int n, unsigned int shift) {
+    assert(shift < 32);
+    unsigned int part1 = n >> shift;
+    unsigned int part2 = n << (32 - shift);
+    return part1 | part2;
+}
+
+unsigned int encrypt(unsigned int n) {
+
+    constexpr unsigned int kMagicNum_1 = 0x42E76A59;
+    constexpr unsigned int kRolBit = 0x0B;
+
+    constexpr unsigned int kMagicNum_2 = 0x89ABCDEF;
+    constexpr unsigned int kRorBit = 0x11;
+
+    unsigned int result = n ^ kMagicNum_1;
+    result = rol_ignore_carry(result, kRolBit);
+    result += kMagicNum_2; 
+    result = ror_ignore_carry(result, kRorBit);
+
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << "=========================\n"
+                 "This is tool for gulong_qun_xia_zhuan. Input a number, it "
+                 "will output the corresponding\n"
+                 "encrypted number which you can use to search with CheatEngine"
+                 ". Input empty string to quit\n"
+              << std::endl;
+
+    for (std::string line; std::getline(std::cin, line);) {
+        if (line.empty()) {
+            break;
+        }
+        try {
+            unsigned long n = std::stoul(line);
+            std::cout << std::hex << encrypt(n) << std::endl;
+        } catch (...) {
+            std::cout << "input string is not a number" << std::endl; 
+        }
+    }
+
+    std::cout << "finish" << std::endl;
+    return 0;
+}
